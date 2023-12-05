ce9198cdcce057e195aca41215012db62a17436a
PierceVDR <piercevdr@gmail.com>
2023-12-05 14:25:46 -0500

    comments

--- scripting.js
+++ scripting.js
@@ -1,4 +1,4 @@
-async function getMessages() {
+async function getMessages() { // Retrieves the JSON for the messages and then puts them on the page
   const url =
     "https://jsonplaceholder.typicode.com/posts";
   const request = new Request(url);
@@ -9,23 +9,24 @@ async function getMessages() {
   fillPage(messages);
 }
 
-function make(type, content, parent) {
+function make(type, content, parent) { // Create the HTML for a component of a single message
     const element = document.createElement(type);
     element.innerHTML=content;
     parent.append(element);
 }
 
-function fillPage(obj) {
+function fillPage(obj) { // Takes the JSON and puts them on the page
 
   for (const msg of obj) {
     const box = document.createElement("div");
     box.className="box";
 
+    // Make the individual HTML elements that'll go in the message
     make("h4", "User #"+msg["userId"], box)
     make("h3", msg["title"], box)
     make("p", msg["body"], box)
 
-    mediaPage.append(box)
+    mediaPage.append(box) // Add that message to the page
   }
 }
 
